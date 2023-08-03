ece8f60b5afe2bddf91817a534a6479a2e00f03c
Rumman Adib <128202186+Rumman023@users.noreply.github.com>
2023-08-03 13:49:09 +0600

    Python Programming - Task 2

--- 210041131_Rumman Adib_Week 2_Task 2.py
+++ 210041131_Rumman Adib_Week 2_Task 2.py
@@ -1,6 +1,20 @@
 import cv2 as cv
 import numpy as np
 
+
+def resize_window(image, result):
+        # Resizing the window with new width and height
+        down_width = 800
+        down_height = 600
+        down_points = (down_width, down_height)
+        resize_down_original = cv.resize(image, down_points, interpolation=cv.INTER_LINEAR)
+        resize_down_rwcolor = cv.resize(result, down_points, interpolation=cv.INTER_LINEAR)
+        
+        # Window pop out
+        cv.imshow('Ankara Messi (Original)', resize_down_original)
+        cv.imshow('Ankara Messi (Red & White Region)', resize_down_rwcolor)
+
+
 def detect_red_and_white_regions(image):
         # Image conversion from BGR to HSV color space
         hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
@@ -29,7 +43,25 @@ def detect_red_and_white_regions(image):
 
 
 def analyze_goat(image_array):
-    pass
+        # Finding the minimumm and maximum pixel values in the image [2]
+        min_value = np.min(image_array)
+        max_value = np.max(image_array)
+
+        # Finding the average pixel value in the image [3]
+        mean_value = np.average(image_array)
+
+        # Finding the total number of non-zero (foreground) pixels in the image [4]
+        nonzero_pixels = np.count_nonzero(image_array)
+
+        # Finding the total number of zero (background) pixels in the image [5]
+        zero_pixels = np.size(image_array) - nonzero_pixels
+
+        # Printing the calculated results on the terminal
+        print("The minimum pixel value in the image is: ", min_value)
+        print("The maximum pixel value in the image is: ", max_value)
+        print("The average pixel value in the image is: ", mean_value)
+        print("The number of foreground pixels is: ", nonzero_pixels)
+        print("The number of background pixels is: ", zero_pixels)
 
 
 
@@ -45,17 +77,9 @@ image_array = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 # Call the analyze_goat function and save the display image
 analyze_goat(image_array)
 
+# Calling the window to execute and show the images
+resize_window(image, result)
 
-
-# Resizing the image with new  width and height
-down_width = 800
-down_height = 600
-down_points = (down_width, down_height)
-resize_down_original = cv.resize(image, down_points, interpolation= cv.INTER_LINEAR)
-resize_down_color = cv.resize(result, down_points, interpolation= cv.INTER_LINEAR)
-
-cv.imshow('Ankara Messi (Original)', resize_down_original)
-cv.imshow('Ankara Messi (Red & White Region)', resize_down_color)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
